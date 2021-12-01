f6845fcaffaa3665c92ddfab6865ccb0f74caffc
xzvf (Péter Bohner) <peter@bohner.me>
2021-12-01 14:30:55 +0100

    Advent Of Code 2021, day01
    Trying to learn clojure this December.

---
+++ adventofcode-2021/day01.clj
@@ -0,0 +1,27 @@
+(ns day01)
+
+(def test-input [199
+                 200
+                 208
+                 210
+                 200
+                 207
+                 240
+                 269
+                 260
+                 263])
+
+(def real-input
+  (map #(Integer/parseInt %) (clojure.string/split (slurp "./inputs/day01.txt") #"\n")))
+
+(defn part1 [input]
+  (apply + (map #(if (< (nth % 0) (nth % 1)) 1 0) (partition 2 1 input))))
+
+(part1 test-input);; => [199 200 208 210 200 207 240 269 260 263])
+(part1 real-input);; => 1616
+
+(defn part2 [input]
+    (part1 (map #(apply + %) (partition 3 1 input))))
+
+(part2 test-input);; => 5
+(part2 real-input);; => 1645
