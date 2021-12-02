34f8f2485c8aeade85afd8acf377dedc399392a4
xzvf (Péter Bohner) <peter@bohner.me>
2021-12-02 10:49:38 +0100

    formatting

--- adventofcode-2021/day01.clj
+++ adventofcode-2021/day01.clj
@@ -15,9 +15,10 @@
   (map #(Integer/parseInt %) (clojure.string/split (slurp "./inputs/day01.txt") #"\n")))
 
 (defn part1 [input]
-  (apply + (map #(if (< (nth % 0) (nth % 1)) 1 0) (partition 2 1 input))))
+  (apply + (map #(if (< (nth % 0) (nth % 1)) 1 0)
+                (partition 2 1 input))))
 
-(part1 test-input);; => [199 200 208 210 200 207 240 269 260 263])
+(part1 test-input);; => 7
 (part1 real-input);; => 1616
 
 (defn part2 [input]
