56ee3dbccac4b7dd9001c36793545d79598328d3
Péter Bohner (xzvf) <peter@bohner.me>
2022-12-02 06:55:50 +0100

    Cleanup AoC day02

--- adventofcode-2022/day02.clj
+++ adventofcode-2022/day02.clj
@@ -8,35 +8,35 @@ C Z
 
 (def real-input (slurp "./inputs/day02.txt"))
 
-(defn parse-input [input]
-  (->>(clojure.string/split input #"\n")
-      (map (fn [x] (clojure.string/split x #" ")))))
+(defn abcxyz-to-num [[a x]]
+  [(cond (= a "A") 1
+         (= a "B") 2
+         :else 3)
+   (cond (= x "X") 1
+         (= x "Y") 2
+         :else 3)])
 
-(defn choice-score [x]
-  (cond (= x "A") 1
-        (= x "B") 2
-        (= x "C") 3))
+(defn parse-input [input]
+  (as-> input x
+    (clojure.string/split x #"\n")
+    (map #(clojure.string/split % #" ") x)
+    (map abcxyz-to-num x)))
 
 (defn beats? [a b]
-  (cond (and (= a "A") (= b "B")) true
-        (and (= a "B") (= b "C")) true
-        (and (= a "C") (= b "A")) true
-        :else false))
+  (or (and (= a 1) (= b 2))
+      (and (= a 2) (= b 3))
+      (and (= a 3) (= b 1))))
 
-(defn to-a [x]
-  (cond (= x "X") "A"
-        (= x "Y") "B"
-        (= x "Z") "C"))
-
-(defn round-score [[a b]]
+(defn result-score [a b]
 (cond (= a b) 3
       (beats? a b) 6
       :else 0))
 
-(defn score [ilist]
-  (apply + (map (fn [[a b]] (+ (round-score [a b])
-                      (choice-score b)))
-       (map (fn [[a b]] [a (to-a b)]) ilist))))
+(defn round-score [[a b]]
+  (+ b (result-score a b)))
+
+(defn score [rounds]
+  (reduce + (map round-score rounds)))
 
 (defn part1 [input]
   (-> input
@@ -44,15 +44,13 @@ C Z
       score))
 
 (defn choose-x [[a b]]
-  (cond (= b "X") (cond (= a "A") "Z"
-                        (= a "B") "X"
-                        (= a "C") "Y")
-        (= b "Y") (cond (= a "A") "X"
-                        (= a "B") "Y"
-                        (= a "C") "Z")
-        (= b "Z") (cond (= a "A") "Y"
-                        (= a "B") "Z"
-                        (= a "C") "X")))
+  (cond (= b 1) (cond (= a 1) 3
+                      (= a 2) 1
+                      (= a 3) 2)
+        (= b 2) a
+        (= b 3) (cond (= a 1) 2
+                      (= a 2) 3
+                      (= a 3) 1)))
 
 (defn part2 [input]
   (->> input
@@ -64,4 +62,3 @@ C Z
 (part1 real-input);; => 13675
 (part2 test-input);; => 12
 (part2 real-input);; => 14184
-
