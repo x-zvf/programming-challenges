18adeb0f8bb6ad2a09dd1ad18b3cd9f526ac2b6f
Péter Bohner (xzvf) <peter@bohner.me>
2024-12-02 13:44:55 +0100

    AOC2024: day02 haskell: cleanup

--- adventofcode-2024/day02.hs
+++ adventofcode-2024/day02.hs
@@ -2,33 +2,30 @@ import System.Environment
 import System.IO
 import Data.List
 
-parse :: String -> [[Int]]
 parse s = map (map read) $ map words $ lines $ s
 
-windows ls = zip ls (tail ls)
-
-allw f g ls = all g $ map f $ windows ls
-
-
 safe ls = mon && delta
-  where mon = allw (uncurry (-)) (<0) ls || allw (uncurry (-)) (>0) ls
-        delta = allw (\(a,b) -> abs (a-b)) (<=3) ls
+  where 
+    windows = zip ls (tail ls)
+    allw f g = all g $ map f $ windows
+    mon = allw (uncurry (-)) (<0) || allw (uncurry (-)) (>0)
+    delta = allw (\(a,b) -> abs (a-b)) (<=3) 
 
-part1 ls = length $ filter safe ls
 
-te [] = []
-te xs = init xs
+dropping ls =
+  map (\i -> let (l,r) = splitAt i ls in (inits l) ++ r) idxs
+  where
+    idxs = [0..(length ls)]
+    inits [] = []
+    inits xs = init xs
 
-dropping ls = map (\i -> let (l,r) = splitAt i ls in (te l) ++ r) [0..(length ls)]
 
-part2 ls = length $ filter (\l -> any safe $ dropping l) ls
-dbg ls = unlines $ map show $ filter (\l -> any safe $ dropping l) ls
+part1 = length . filter safe
+part2 = length . filter (\l -> any safe $ dropping l) 
 
 main = do
     args <- getArgs
     s <- readFile (args !! 0)
-
     let ls = parse s
-    --putStrLn $ show $ part1 ls
-    --putStrLn $ show $ part2 ls
-    putStrLn $  dbg ls
+    putStrLn $ show $ part1 ls
+    putStrLn $ show $ part2 ls
