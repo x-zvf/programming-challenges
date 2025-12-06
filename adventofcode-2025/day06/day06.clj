35393027daf4497ec56203e0470619b0219ed5e5
Péter Bohner (xzvf) <peter@bohner.me>
2025-12-06 11:08:51 +0100

    all the code reuse

--- adventofcode-2025/day06/day06.clj
+++ adventofcode-2025/day06/day06.clj
@@ -2,29 +2,10 @@
   (:require [clojure.string :as string]
             [clojure.edn :as edn]))
 
-(def sample (slurp "./sample.txt"))
-(def input (slurp "./input.txt"))
-
 (defn transpose [m]
   (apply mapv vector m))
 
-(defn part1 [file]
-  (->> file
-       (string/split-lines)
-       (mapv (fn [line]
-               (->> line
-                    (#(string/split % #" +"))
-                    (filter #(not (empty? %)))
-                    (mapv edn/read-string))))
-       transpose
-       (map reverse)
-       (cons +)
-       eval))
-
-(part1 sample) ; 4277556
-(part1 input) ; 4405895212738
-
-(defn part2 [file]
+(defn solve [file part2]
   (let [lines (->> file string/split-lines (mapv vec))
         number-lines (drop-last lines)
         ops-line (last lines)
@@ -33,8 +14,9 @@
         (->> ops-line
              (keep-indexed #(if (not= \space %2) [%1 %2] nil))
              transpose)
-        ops (map #(edn/read-string (str %)) ops)
         split-idxs (conj split-idxs (count (first number-lines)))
+        ops (map #(edn/read-string (str %)) ops)
+
         blocks (mapv (fn [line]
                        (mapv (fn [[f t]] (subvec line f t))
                              (partition 2 1 split-idxs)))
@@ -44,12 +26,16 @@
           (fn [i op]
             (->> blocks
                  (mapv #(nth % i))
-                 transpose
+                 ((if part2 transpose identity))
                  (map #(edn/read-string (apply str %)))
                  (filter #(not (nil? %)))
                  (cons op)
                  eval)))
          (apply +))))
 
-(part2 sample) ; 3263827
-(part2 input)  ; 7450962489289
+(def sample (slurp "./sample.txt"))
+(def input (slurp "./input.txt"))
+(solve sample false) ; 4277556
+(solve input false) ; 4405895212738
+(solve sample true) ; 3263827
+(solve input true)  ; 7450962489289
